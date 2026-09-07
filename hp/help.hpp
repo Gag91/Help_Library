2f0d46bcf80d631c8bfd1e094c2d6dc34db76a1e
Gag91 <rzm664477@gmail.com>
2026-09-07 22:20:22 +0200

    added command.hpp include in help.hpp

--- hp/help.hpp
+++ hp/help.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
 #include "hp/colors/color.hpp"
-#include "hp/math/math.hpp"
-#include "hp/string/string.hpp"
-#include "hp/random/random.hpp"
-#include "hp/keyboard/keyboard.hpp"
-#include "hp/menu/menu.hpp"
-#include "hp/fields/inputField.hpp"
-#include "hp/time/inputs.hpp"
-#include "hp/other/other.hpp"
 #include "hp/containers/containers.hpp"
+#include "hp/fields/inputField.hpp"
+#include "hp/keyboard/keyboard.hpp"
+#include "hp/math/math.hpp"
+#include "hp/menu/menu.hpp"
+#include "hp/other/other.hpp"
+#include "hp/random/random.hpp"
 #include "hp/serializer/serialize.hpp"
+#include "hp/string/string.hpp"
+#include "hp/system/command.hpp"
+#include "hp/time/inputs.hpp"
+
